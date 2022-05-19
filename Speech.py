b3bb55fc30bdd077bc13c04649437822a3b25038
Harshal Dharpure <63531290+harshalDharpure@users.noreply.github.com>
2022-05-19 15:58:00 +0530

    Create search,py

---
+++ Speech.py
@@ -0,0 +1,10 @@
+from gtts import gTTS
+import os
+
+mytext="Hello My name is Harshal Dharpure"
+language='en'
+
+output=gTTS(text=mytext,lang=language,slow=False)
+output.save("output.mp3")
+
+os.system("start output.mp3")
